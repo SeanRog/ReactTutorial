0d039757a651ffb42190b84cb6a0ddb0f920cd26
SeanRog <seanmikerog@gmail.com>
2021-03-14 17:44:38 -0500

    Reusable components: Wrapped CommentDetail component inside ApprovalCard component

--- components-tutorial/src/index.js
+++ components-tutorial/src/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './components/CommentDetail';
+import ApprovalCard from './components/ApprovalCard';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                <img alt="avatar" src='https://source.unsplash.com/random' />
-                </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">Nice blog post!</div>
-            </div>
-        </div>
-        </div>
-    )
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45" content="Nice blog post!" avatar="https://source.unsplash.com/user/erondu" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00" content="ok blog post" avatar="https://images.unsplash.com/photo-1582378944540-7f8685722d32?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1456&q=80"  />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 4:32" content="bad blog post :(" avatar="https://source.unsplash.com/random" />
+      </ApprovalCard>
+    </div>
+  )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
