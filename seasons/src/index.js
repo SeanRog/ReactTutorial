f8b0ed63d228731a4a104cb91d62638ac474de2b
SeanRog <seanmikerog@gmail.com>
2021-03-15 07:38:05 -0500

    small change

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,21 +5,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: null
     };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({lat: position.coords.latitude}),
-      (err) => console.log(err)
+      (position) => {this.setState({
+        lat: position.coords.latitude,
+        errorMessage: null
+      })},
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+          lat: null
+        })
+      }
     );
   }
 
   render() {
-    
-    return (
-      <div>
-        Latitude: {this.state.lat}
-      </div>
-    )
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    return <div>Loading!</div>;
   }
 }
 
